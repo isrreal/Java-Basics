c3632f66bf95ca1964cc9158df37005e8cda52a7
israel souza <souzaferreira437@gmail.com>
2022-05-03 19:54:07 -0300

    for each atualizado

--- Sintaxe/Basico/Loops/ForEach.java
+++ Sintaxe/Basico/Loops/ForEach.java
@@ -1,15 +1,16 @@
+import java.util.ArrayList;
 public class ForEach {
-    public static void main ( String args[]) {
-        String[] nomes = { "Israel", "Jeronimo", "Vanessa", "Valeria" };
-
-        for (String jooj: nomes){       // Sintaxe:   Tipo nomedavariavel : container { }
+    public static void main (String args[]) {
+        ArrayList<String> nomes = new ArrayList<String>();
+        nomes.add("israel");
+        nomes.add("jooj");
+        for (String jooj: nomes){       // Sintaxe:   TipoDoContainer nomedavariavel : container { }
                                         /* 
                                             OBS 1:  No for each, para "usar" o container, utiliza o nome da                                             
                                             variavel criada durante a criação do loop, uma copia do container, 
                                             não o nome do container em si
-                                        */        
-                                        // OBS 2: Só é possível fazer um forEach com containers primitivos 
-            System.out.println(jooj);   // veja que foi usado o nome da variável, não o do container
+                                        */         
+            System.out.println(jooj);   
         }
     }    
 }
